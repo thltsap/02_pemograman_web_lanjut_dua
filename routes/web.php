659ff849fdb2d98e72acdc7f6dba3870ba3f274b
ThalitaSaniyyaAqillaPutri <thalitakilla1207@gmail.com>
2022-09-06 20:29:26 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\PageControl;
+// use App\Http\Controllers\PageControl;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'about']);
 
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+Route::get('/articles/{id}', [ArticleController::class, 'articles'])
+;
+
 
